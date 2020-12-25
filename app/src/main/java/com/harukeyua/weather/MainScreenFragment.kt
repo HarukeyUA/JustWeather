870afeb994f4cd6070445f2adf696d573e411344
Nazar Rusnak <inazarrusnak@gmail.com>
2020-12-25 14:13:23 +0200

    Setup navigation component and hilt

--- app/src/main/java/com/harukeyua/weather/MainScreenFragment.kt
+++ app/src/main/java/com/harukeyua/weather/MainScreenFragment.kt
@@ -16,14 +16,20 @@
 
 package com.harukeyua.weather
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main_screen, container, false)
     }
 }
